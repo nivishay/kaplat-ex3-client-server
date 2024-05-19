67e522129e23bc4f71bbbc89d76a4ecab1aa29b0
Niv Ishay <nivi.ishay@gmail.com>
2024-05-20 01:18:26 +0300

    shell of functions for the srever.(the names of the functions)

--- my_server_test.js
+++ my_server_test.js
@@ -1,27 +1,47 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8574;
 
 const testMiddleware = (req, res, next) =>{ //test middleware
-    req.url = '/test' + req.url;
+    req.url = '/book' + req.url;
     next();
   };
 
-app.get('/test_get_method',testMiddleware, (req, res) => {
-    //Accessing query parameters
-    const id = req.query.id;
-    const year = req.query.year;
+  //This is a sanity endpoint used to check that the server is up and running.
+  app.get('/books/health', (req, res) => {
+    res.status(200).send("OK");
+  });
 
-    //Sending a response with the query parameters
-    res.send(`Hello, ${id}! You are ${year} years old.`);
-  });
-  app.post('/test_post_method', (req, res) => {
-    const data = req.body;
-    console.log(data);
-    res.status(200).json({ message: 'Data updated successfully' });
-  });
+  //Creates a new Book in the system
+  //When a new Book is created, it is assigned by the server the next id in turn.
+app.post('/book', (req, res) => {
+
+});
+
+//Returns the total number of Books in the system, according to optional filters.
+app.get('/books/total', (req, res) => {
+  
+})
+//Returns the content of the books according to the given filters as described by the total endpoint
+app.get('/books', (req, res) => {
+
+});
+
+//Gets a single book’s data according to its id
+app.get('/book', (req, res) => {
+
+});
+//Updates given book’s price
+app.put('/book', (req, res) => {
+  
+})
+//Deletes a Book object.
+app.delete('/book', (req, res) => {
+  
+})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
